bhzhu203/bcachefs

bcachefs: Add __GFP_RECLAIMABLE to remaining allocations in IO and btree paths
Additional allocations that were missing __GFP_RECLAIMABLE:

1. btree/cache.c: __vmalloc fallback for btree node data - vmalloc in
   kernel 6.17+ supports __GFP_RECLAIMABLE, pages land in
   MIGRATE_RECLAIMABLE pageblocks.

2. btree/iter.c: transaction path reallocation (kvzalloc) - btree
   transaction structures, typically 4-8KB per transaction.

3. data/compress.c: page pointer array for vmap in decompression path -
   allocated when bio has more than 16 pages.

These allocations, while not as frequent as bio data pages, can still
contribute to memory fragmentation when they occur during high I/O load.

Signed-off-by: bhzhu <bhzhu@kernel.org>

fs/bcachefs/data/compress.c
@@ -174,7 +174,7 @@ static struct bbuf __bio_map_or_bounce(struct bch_fs *c, struct bio *bio,
 
 	struct page *stack_pages[16];
 	struct page **pages = nr_pages > ARRAY_SIZE(stack_pages)
-		? kmalloc_array(nr_pages, sizeof(struct page *), GFP_NOFS|__GFP_NORETRY)
+		? kmalloc_array(nr_pages, sizeof(struct page *), GFP_NOFS|__GFP_RECLAIMABLE|__GFP_NORETRY)
 		: stack_pages;
 	if (!pages)
 		return bio_bounce(c, bio, start, rw);

bhzhu203/bcachefs-tools

fs/data/compress.c
@@ -204,7 +204,7 @@ static struct bbuf __bch2_bio_map_or_bounce(struct bch_fs *c, struct bio *bio,
 
 	struct page *stack_pages[16];
 	struct page **pages = nr_pages > ARRAY_SIZE(stack_pages)
-		? kmalloc_array(nr_pages, sizeof(struct page *), GFP_NOIO|__GFP_NORETRY)
+		? kmalloc_array(nr_pages, sizeof(struct page *), GFP_NOIO|__GFP_RECLAIMABLE|__GFP_NORETRY)
 		: stack_pages;
 	if (!pages)
 		return bch2_bio_bounce(c, bio, start, rw);