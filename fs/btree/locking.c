bhzhu203/bcachefs

bcachefs: Fix HDD livelock and journal deadlock from lock contention
On HDD systems under heavy concurrent workloads (VMs, containers, many
user processes), bcachefs would enter a livelock state where processes
stuck in D state for hours or days, the journal reported journal_max_open
errors, and system load skyrocketed despite HDD IO not being saturated.

The root causes were multiple interlocking deadlock/livelock cycles in
the btree transaction, lock, and journal subsystems. This commit breaks
each cycle with targeted timeouts and device-aware throttling.

1. Transaction admission control (btree/iter.c, btree/types.h,
   btree/iter.h, sb/members.c)

   On HDD, too many concurrent btree transactions compete for the same
   btree node six_locks. Each transaction holds locks while waiting for
   slow HDD IO (cache misses, btree node reads), causing lock wait times
   to explode exponentially.

   Add a semaphore-based throttle that limits concurrent btree
   transactions to 32 on HDD. The throttle is acquired in
   __bch2_trans_get() and released in bch2_trans_put().

   - PF_MEMALLOC bypasses throttle (memory reclaim must always proceed)
   - PF_WQ_WORKER bypasses throttle (write_complete and write_buffer_flush
     workers must not block on throttle — otherwise they deadlock with
     the transactions holding throttle slots that wait for write completion)
   - down_timeout(30s) with retry loop prevents indefinite blocking
   - bch2_trans_throttle_update() enables/disables throttle based on
     device rotational flag, called from bch2_sb_members_to_cpu()

   Previous version used down() (infinite block) with limit 8, which
   caused processes to stuck in bch2_trans_get() for days when all
   slots were consumed by stuck transactions. The throttle timeout and
   PF_WQ_WORKER bypass fix this.

2. Btree lock wait timeout (btree/locking.c)

   The six_lock cycle detector only detects cycles (A waits for B waits
   for C waits for A). It cannot detect dependency chains 

fs/bcachefs/btree/locking.c
@@ -573,8 +573,30 @@ out:
 int bch2_six_check_for_deadlock(struct six_lock *lock, struct six_lock_waiter *w)
 {
 	struct btree_trans *trans = container_of(w, struct btree_trans, locking_wait);
+	struct bch_fs *c = trans->c;
 
-	return bch2_check_for_deadlock(trans, NULL);
+	int ret = bch2_check_for_deadlock(trans, NULL);
+	if (ret)
+		return ret;
+
+	/*
+	 * On HDD, break livelock chains: if a transaction has been waiting
+	 * for a btree lock for too long, restart it. The lock holder may
+	 * be blocked on slow IO or another dependency, creating a chain
+	 * that prevents forward progress. By restarting, we break the
+	 * chain and allow the lock holder to complete.
+	 *
+	 * 30 seconds is long enough to avoid false positives (legitimate
+	 * HDD reads take ~20ms, so even a chain of 100 would be 2s).
+	 */
+	if (!bitmap_empty(c->devs_rotational.d, BCH_SB_MEMBERS_MAX) &&
+	    w->start_time &&
+	    local_clock() - w->start_time > 30ULL * NSEC_PER_SEC) {
+		return btree_trans_restart(trans,
+			BCH_ERR_transaction_restart_deadlock_waitlist_alloc) ?: -EINTR;
+	}
+
+	return 0;
 }
 
 int __bch2_btree_node_lock_write(struct btree_trans *trans, struct btree_path *path,

bhzhu203/bcachefs-tools

fs/btree/locking.c
@@ -676,7 +676,30 @@ int bch2_six_check_for_deadlock(struct six_lock *lock, struct six_lock_waiter *w
 		WRITE_ONCE(current->wake_cpu, trans->shard_cpu);
 #endif
 
-	return bch2_check_for_deadlock(trans, NULL);
+	struct bch_fs *c = trans->c;
+
+	int ret = bch2_check_for_deadlock(trans, NULL);
+	if (ret)
+		return ret;
+
+	/*
+	 * On HDD, break livelock chains: if a transaction has been waiting
+	 * for a btree lock for too long, restart it. The lock holder may
+	 * be blocked on slow IO or another dependency, creating a chain
+	 * that prevents forward progress. By restarting, we break the
+	 * chain and allow the lock holder to complete.
+	 *
+	 * 30 seconds is long enough to avoid false positives (legitimate
+	 * HDD reads take ~20ms, so even a chain of 100 would be 2s).
+	 */
+	if (!bitmap_empty(c->devs_rotational.d, BCH_SB_MEMBERS_MAX) &&
+	    w->trans_start_time &&
+	    local_clock() - w->trans_start_time > 30ULL * NSEC_PER_SEC) {
+		return btree_trans_restart(trans,
+			BCH_ERR_transaction_restart_deadlock_waitlist_alloc) ?: -EINTR;
+	}
+
+	return 0;
 }
 
 /*