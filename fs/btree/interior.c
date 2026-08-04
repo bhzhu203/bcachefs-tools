bhzhu203/bcachefs

bcachefs: Add __GFP_RECLAIMABLE to remaining hot path allocations and darray defaults
Add __GFP_RECLAIMABLE to ~100+ allocation sites across 31 files covering
all remaining high and medium frequency UNMOVABLE allocation paths:

- vfs/pagecache.c: __bch2_folio_create internal gfp (covers all bch_folio
  allocations, one per page cache folio — the largest hidden UNMOVABLE source)
- btree/iter.c: __bch2_trans_kmalloc primary kmalloc (hottest path, used by
  every btree update) and trans mempool
- vfs/buffered.c, vfs/direct.c, vfs/io.c: all read/write/direct IO bios,
  writepage state, folio reservation, nocow flush
- data/read.c, data/write.c, data/ec/: read bounce/clone, write replica/
  new/split, EC stripe bios, promote_op, keylist, data_update bvecs
- btree/read.c, btree/write.c, journal/write.c: btree node and journal bios
- journal/init.c: pin fifo, bucket_seq, buckets arrays
- btree/interior.c, btree/bkey_buf.h, btree/check.c: interior_updates
  mempool, async_btree_rewrite, bkey_buf fallback, gc buckets
- util/darray.h: change default gfp for darray_push/darray_make_room/
  darray_resize/darray_resize_rcu macros to GFP_KERNEL|__GFP_RECLAIMABLE
  (blanket coverage for write_buffer, btree interior, journal_overlay,
  accounting, key_cache, and all other darray callers)
- init/fs.c, init/dev.c: bch_fs and bch_dev struct allocations
- alloc/replicas.c, alloc/backpointers.c, fs/quota.c, snapshots/snapshot.c,
  data/copygc.c, data/reflink.c, data/nocow_locking.c, util/six.c

__GFP_RECLAIMABLE only changes pageblock migratetype from MIGRATE_UNMOVABLE
to MIGRATE_RECLAIMABLE without altering allocation behavior (retry, reclaim,
IO/FS allowances). This enables kcompactd to migrate these pages during
compaction, reducing order-9 fragmentation that causes I/O storms on HDD.

fs/bcachefs/btree/interior.c
@@ -1428,7 +1428,7 @@ bch2_btree_update_start(struct btree_trans *trans, struct btree_path *path,
 		}
 	}
 
-	as = mempool_alloc(&c->btree.interior_updates.pool, GFP_NOFS);
+	as = mempool_alloc(&c->btree.interior_updates.pool, GFP_NOFS|__GFP_RECLAIMABLE);
 	memset(as, 0, sizeof(*as));
 	closure_init(&as->cl, NULL);
 	as->c			= c;
@@ -3335,7 +3335,7 @@ static void async_btree_node_rewrite_work(struct work_struct *work)
 void bch2_async_btree_op(struct bch_fs *c, struct btree *b,
 				enum async_btree_op op)
 {
-	struct async_btree_rewrite *a = kzalloc(sizeof(*a), GFP_NOFS);
+	struct async_btree_rewrite *a = kzalloc(sizeof(*a), GFP_NOFS|__GFP_RECLAIMABLE);
 	if (!a)
 		return;
 

fs/bcachefs/btree/iter.c
@@ -3567,7 +3567,7 @@ void *__bch2_trans_kmalloc(struct btree_trans *trans, size_t size, unsigned long
 
 	bool lock_dropped = false;
 	new_mem = allocate_dropping_locks_norelock(trans, lock_dropped,
-					kmalloc(new_bytes, _gfp|__GFP_NOWARN|__GFP_NORETRY));
+					kmalloc(new_bytes, _gfp|__GFP_NOWARN|__GFP_NORETRY|__GFP_RECLAIMABLE));
 	if (!new_mem) {
 		new_mem = mempool_alloc(&c->btree.trans.malloc_pool, GFP_KERNEL|__GFP_RECLAIMABLE|__GFP_NORETRY);
 		new_bytes = BTREE_TRANS_MEM_MAX;
@@ -3753,7 +3753,7 @@ static inline struct btree_trans *bch2_trans_alloc(struct bch_fs *c)
 		}
 	}
 
-	struct btree_trans *trans = mempool_alloc(&c->btree.trans.pool, GFP_NOFS);
+	struct btree_trans *trans = mempool_alloc(&c->btree.trans.pool, GFP_NOFS|__GFP_RECLAIMABLE);
 	memset(trans, 0, sizeof(*trans));
 
 	seqmutex_lock(&c->btree.trans.lock);
@@ -3925,7 +3925,7 @@ skip_throttle:
 		if (s->max_mem) {
 			unsigned expected_mem_bytes = roundup_pow_of_two(s->max_mem);
 
-			trans->mem = kmalloc(expected_mem_bytes, GFP_KERNEL|__GFP_NOWARN|__GFP_NORETRY);
+			trans->mem = kmalloc(expected_mem_bytes, GFP_KERNEL|__GFP_NOWARN|__GFP_NORETRY|__GFP_RECLAIMABLE);
 			if (likely(trans->mem))
 				trans->mem_bytes = expected_mem_bytes;
 		}

fs/bcachefs/data/keylist.c
@@ -18,7 +18,7 @@ int bch2_keylist_realloc(struct keylist *l, u64 *inline_u64s,
 	    (old_buf && roundup_pow_of_two(oldsize) == newsize))
 		return 0;
 
-	new_keys = krealloc(old_buf, sizeof(u64) * newsize, GFP_NOFS|__GFP_NORETRY);
+	new_keys = krealloc(old_buf, sizeof(u64) * newsize, GFP_NOFS|__GFP_NORETRY|__GFP_RECLAIMABLE);
 	if (!new_keys)
 		return -ENOMEM;
 

bhzhu203/bcachefs-tools

fs/btree/interior.c
@@ -1507,7 +1507,7 @@ bch2_btree_update_start(struct btree_trans *trans, btree_path_idx_t path_idx,
 		}
 	}
 
-	as = mempool_alloc(&c->btree.interior_updates.pool, GFP_NOIO);
+	as = mempool_alloc(&c->btree.interior_updates.pool, GFP_NOIO|__GFP_RECLAIMABLE);
 	memset(as, 0, sizeof(*as));
 	closure_init(&as->cl, NULL);
 	as->c			= c;
@@ -3517,7 +3517,7 @@ void bch2_async_btree_op(struct bch_fs *c, struct btree *b,
 		return;
 
 	struct async_btree_rewrite *a =
-		kzalloc(sizeof(*a), GFP_NOIO|__GFP_NORETRY|__GFP_NOWARN);
+		kzalloc(sizeof(*a), GFP_NOIO|__GFP_NORETRY|__GFP_NOWARN|__GFP_RECLAIMABLE);
 	if (!a)
 		return;
 

fs/btree/iter.c
@@ -3700,7 +3700,7 @@ void *__bch2_trans_kmalloc(struct btree_trans *trans, size_t size, unsigned long
 
 	bool lock_dropped = false;
 	new_mem = allocate_dropping_locks_norelock(trans, lock_dropped,
-					kmalloc(new_bytes, _gfp|__GFP_NOWARN|__GFP_NORETRY));
+					kmalloc(new_bytes, _gfp|__GFP_NOWARN|__GFP_NORETRY|__GFP_RECLAIMABLE));
 	if (!new_mem) {
 		new_mem = mempool_alloc(&c->btree.trans.malloc_pool, GFP_KERNEL|__GFP_RECLAIMABLE|__GFP_NORETRY);
 		new_bytes = BTREE_TRANS_MEM_MAX;
@@ -3916,7 +3916,7 @@ static inline struct btree_trans *bch2_trans_alloc(struct bch_fs *c)
 		}
 	}
 
-	struct btree_trans *trans = mempool_alloc(&c->btree.trans.pool, GFP_NOIO);
+	struct btree_trans *trans = mempool_alloc(&c->btree.trans.pool, GFP_NOIO|__GFP_RECLAIMABLE);
 	memset(trans, 0, sizeof(*trans));
 
 	seqmutex_lock(&c->btree.trans.lock);
@@ -4101,7 +4101,7 @@ skip_throttle:
 		if (s->max_mem) {
 			unsigned expected_mem_bytes = roundup_pow_of_two(s->max_mem);
 
-			trans->mem = kmalloc(expected_mem_bytes, GFP_KERNEL|__GFP_NOWARN|__GFP_NORETRY);
+			trans->mem = kmalloc(expected_mem_bytes, GFP_KERNEL|__GFP_NOWARN|__GFP_NORETRY|__GFP_RECLAIMABLE);
 			if (likely(trans->mem))
 				trans->mem_bytes = expected_mem_bytes;
 		}

fs/data/keylist.c
@@ -18,7 +18,7 @@ int bch2_keylist_realloc(struct keylist *l, u64 *inline_u64s,
 	    (old_buf && roundup_pow_of_two(oldsize) == newsize))
 		return 0;
 
-	new_keys = krealloc(old_buf, sizeof(u64) * newsize, GFP_NOIO|__GFP_NORETRY);
+	new_keys = krealloc(old_buf, sizeof(u64) * newsize, GFP_NOIO|__GFP_NORETRY|__GFP_RECLAIMABLE);
 	if (!new_keys)
 		return -ENOMEM;
 