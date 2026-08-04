bhzhu203/bcachefs

bcachefs: Prevent compaction triggers and protect btree cache on HDD
Add __GFP_NORETRY to all direct kmalloc/krealloc/mempool_alloc calls
across bcachefs so that allocation failures return NULL immediately
instead of triggering direct compaction. This prevents bcachefs from
waking kcompactd0 under memory pressure, which was causing 83%+ CPU
usage and system-wide stalls on 16GB HDD systems.

kvmalloc/kvzalloc calls are unaffected because kvmalloc internally
removes __GFP_DIRECT_RECLAIM via kmalloc_gfp_adjust() for high-order
allocations, so they already cannot trigger compaction.

Also raise the btree cache shrinker seeks from 4 to 6 on HDD so that
when kcompactd is woken by other processes, the kernel prefers
reclaiming page cache (cheap sequential re-reads) over btree nodes
(expensive 10ms+ random seeks). This preserves btree cache hit rate
and prevents IO spikes under memory pressure.

No proactive btree cache shrinking is performed - the cache is kept
full for optimal hit rate. The combination of __GFP_NORETRY (prevents
bcachefs from triggering compaction) and higher seeks (protects cache
when compaction is triggered by others) ensures bcachefs maintains
high memory priority without degrading system stability.

Files changed:
- btree/iter.c: trans memory allocations (up to 64KB)
- data/compress.c: compression bounce buffers and workspace pools
- btree/sort.c: btree sort bounce pool
- data/keylist.c: key list reallocation
- data/update.c: bio vector arrays
- data/write.c: bio bounce buffer pool
- btree/cache.c: shrinker seeks 4 -> 6 for HDD

fs/bcachefs/btree/sort.c
@@ -255,7 +255,7 @@ void *bch2_btree_bounce_alloc(struct bch_fs *c, size_t size, bool *used_mempool)
 	void *p = kvmalloc(size, GFP_NOWAIT|__GFP_ACCOUNT|__GFP_RECLAIMABLE);
 	if (!p) {
 		*used_mempool = true;
-		p = mempool_alloc(&c->btree.bounce_pool, GFP_NOFS|__GFP_ACCOUNT|__GFP_RECLAIMABLE);
+		p = mempool_alloc(&c->btree.bounce_pool, GFP_NOFS|__GFP_ACCOUNT|__GFP_RECLAIMABLE|__GFP_NORETRY);
 	}
 	return p;
 }

fs/bcachefs/data/compress.c
@@ -97,11 +97,11 @@ static struct bbuf __bounce_alloc(struct bch_fs *c, unsigned size, int rw)
 
 	BUG_ON(size > c->opts.encoded_extent_max);
 
-	b = kmalloc(size, GFP_NOFS|__GFP_NOWARN|__GFP_RECLAIMABLE);
+	b = kmalloc(size, GFP_NOFS|__GFP_NOWARN|__GFP_RECLAIMABLE|__GFP_NORETRY);
 	if (b)
 		return (struct bbuf) { .c = c, .b = b, .type = BB_kmalloc, .rw = rw };
 
-	b = mempool_alloc(&c->compress.bounce[rw], GFP_NOFS);
+	b = mempool_alloc(&c->compress.bounce[rw], GFP_NOFS|__GFP_NORETRY);
 	if (b)
 		return (struct bbuf) { .c = c, .b = b, .type = BB_mempool, .rw = rw };
 
@@ -174,7 +174,7 @@ static struct bbuf __bio_map_or_bounce(struct bch_fs *c, struct bio *bio,
 
 	struct page *stack_pages[16];
 	struct page **pages = nr_pages > ARRAY_SIZE(stack_pages)
-		? kmalloc_array(nr_pages, sizeof(struct page *), GFP_NOFS)
+		? kmalloc_array(nr_pages, sizeof(struct page *), GFP_NOFS|__GFP_NORETRY)
 		: stack_pages;
 	if (!pages)
 		return bio_bounce(c, bio, start, rw);
@@ -245,7 +245,7 @@ static int buf_uncompress(struct bch_fs *c,
 			.avail_out	= dst_len,
 		};
 
-		void *workspace = mempool_alloc(workspace_pool, GFP_NOFS);
+		void *workspace = mempool_alloc(workspace_pool, GFP_NOFS|__GFP_NORETRY);
 
 		zlib_set_workspace(&strm, workspace);
 		zlib_inflateInit2(&strm, -MAX_WBITS);
@@ -264,7 +264,7 @@ static int buf_uncompress(struct bch_fs *c,
 		if (real_src_len > src_len - 4)
 			return bch_err_throw(c, decompress_zstd_src_len_bad);
 
-		void *workspace = mempool_alloc(workspace_pool, GFP_NOFS);
+		void *workspace = mempool_alloc(workspace_pool, GFP_NOFS|__GFP_NORETRY);
 		ctx = zstd_init_dctx(workspace, zstd_dctx_workspace_bound());
 
 		size_t ret = zstd_decompress_dctx(ctx,
@@ -490,7 +490,7 @@ static unsigned bch2_compress(struct bch_fs *c,
 		}
 	}
 
-	void *workspace = mempool_alloc(workspace_pool, GFP_NOFS);
+	void *workspace = mempool_alloc(workspace_pool, GFP_NOFS|__GFP_NORETRY);
 
 	/*
 	 * XXX: this algorithm sucks when the compression code doesn't tell us

fs/bcachefs/data/keylist.c
@@ -18,7 +18,7 @@ int bch2_keylist_realloc(struct keylist *l, u64 *inline_u64s,
 	    (old_buf && roundup_pow_of_two(oldsize) == newsize))
 		return 0;
 
-	new_keys = krealloc(old_buf, sizeof(u64) * newsize, GFP_NOFS);
+	new_keys = krealloc(old_buf, sizeof(u64) * newsize, GFP_NOFS|__GFP_NORETRY);
 	if (!new_keys)
 		return -ENOMEM;
 

bhzhu203/bcachefs-tools

fs/btree/sort.c
@@ -266,7 +266,7 @@ void *bch2_btree_bounce_alloc_noprof(struct bch_fs *c, size_t size, bool *used_m
 #endif
 	if (!p) {
 		*used_mempool = true;
-		p = mempool_alloc_noprof(&c->btree.bounce_pool, GFP_NOIO|__GFP_ACCOUNT|__GFP_RECLAIMABLE);
+		p = mempool_alloc_noprof(&c->btree.bounce_pool, GFP_NOIO|__GFP_ACCOUNT|__GFP_RECLAIMABLE|__GFP_NORETRY);
 	}
 	return p;
 }

fs/data/compress.c
@@ -124,11 +124,11 @@ struct bbuf bch2_bounce_alloc(struct bch_fs *c, unsigned size, int rw)
 	 * The mempool fallback below is not flagged: mempool reuse returns
 	 * elements without re-zeroing, so it never pays the init_on_alloc cost.
 	 */
-	b = kmalloc(size, GFP_NOIO|__GFP_NOWARN|__GFP_SKIP_ZERO);
+	b = kmalloc(size, GFP_NOIO|__GFP_NOWARN|__GFP_SKIP_ZERO|__GFP_RECLAIMABLE|__GFP_NORETRY);
 	if (b)
 		return (struct bbuf) { .c = c, .b = b, .type = BB_kmalloc, .rw = rw };
 
-	b = mempool_alloc(&c->compress.bounce[rw], GFP_NOIO);
+	b = mempool_alloc(&c->compress.bounce[rw], GFP_NOIO|__GFP_NORETRY);
 	if (b)
 		return (struct bbuf) { .c = c, .b = b, .type = BB_mempool, .rw = rw };
 
@@ -204,7 +204,7 @@ static struct bbuf __bch2_bio_map_or_bounce(struct bch_fs *c, struct bio *bio,
 
 	struct page *stack_pages[16];
 	struct page **pages = nr_pages > ARRAY_SIZE(stack_pages)
-		? kmalloc_array(nr_pages, sizeof(struct page *), GFP_NOIO)
+		? kmalloc_array(nr_pages, sizeof(struct page *), GFP_NOIO|__GFP_NORETRY)
 		: stack_pages;
 	if (!pages)
 		return bch2_bio_bounce(c, bio, start, rw);
@@ -277,7 +277,7 @@ int bch2_buf_uncompress(struct bch_fs *c,
 			.avail_out	= dst_len,
 		};
 
-		void *workspace = mempool_alloc(workspace_pool, GFP_NOIO);
+		void *workspace = mempool_alloc(workspace_pool, GFP_NOIO|__GFP_NORETRY);
 
 		zlib_set_workspace(&strm, workspace);
 		zlib_inflateInit2(&strm, -MAX_WBITS);
@@ -298,7 +298,7 @@ int bch2_buf_uncompress(struct bch_fs *c,
 		if (real_src_len > src_len - 4)
 			return bch_err_throw(c, decompress_zstd_src_len_bad);
 
-		void *workspace = mempool_alloc(workspace_pool, GFP_NOIO);
+		void *workspace = mempool_alloc(workspace_pool, GFP_NOIO|__GFP_NORETRY);
 		ctx = zstd_init_dctx(workspace, zstd_dctx_workspace_bound());
 
 		size_t ret = zstd_decompress_dctx(ctx,
@@ -505,7 +505,7 @@ static unsigned bch2_compress(struct bch_fs *c,
 		}
 	}
 
-	void *workspace = mempool_alloc(workspace_pool, GFP_NOIO);
+	void *workspace = mempool_alloc(workspace_pool, GFP_NOIO|__GFP_NORETRY);
 
 	/*
 	 * XXX: this algorithm sucks when the compression code doesn't tell us

fs/data/keylist.c
@@ -18,7 +18,7 @@ int bch2_keylist_realloc(struct keylist *l, u64 *inline_u64s,
 	    (old_buf && roundup_pow_of_two(oldsize) == newsize))
 		return 0;
 
-	new_keys = krealloc(old_buf, sizeof(u64) * newsize, GFP_NOIO);
+	new_keys = krealloc(old_buf, sizeof(u64) * newsize, GFP_NOIO|__GFP_NORETRY);
 	if (!new_keys)
 		return -ENOMEM;
 