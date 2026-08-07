bhzhu203/bcachefs

bcachefs: Add __GFP_RECLAIMABLE to bio data page and compression workspace allocations
The read I/O path was allocating bio data pages and compression workspaces
without __GFP_RECLAIMABLE, causing them to be placed in MIGRATE_UNMOVABLE
pageblocks. These temporary buffers (used during readahead and decompression)
were contributing to memory fragmentation and kcompactd failures.

Fixed locations:
- bch2_bio_alloc_pages() for read bounce buffers (data/write.c)
- bio_bounce_buf mempool fallback (data/write.c)
- Compress bounce buffer mempool fallback (data/compress.c)
- gzip/zstd decompression workspace mempools (data/compress.c)
- bio_bounce_buf_alloc_fn backing allocator (data/read.c)

All these allocations are short-lived I/O buffers that should use
MIGRATE_RECLAIMABLE pageblocks to reduce fragmentation impact on
kcompactd.

Signed-off-by: bhzhu <bhzhu@kernel.org>

fs/bcachefs/data/compress.c
@@ -101,7 +101,7 @@ static struct bbuf __bounce_alloc(struct bch_fs *c, unsigned size, int rw)
 	if (b)
 		return (struct bbuf) { .c = c, .b = b, .type = BB_kmalloc, .rw = rw };
 
-	b = mempool_alloc(&c->compress.bounce[rw], GFP_NOFS|__GFP_NORETRY);
+	b = mempool_alloc(&c->compress.bounce[rw], GFP_NOFS|__GFP_RECLAIMABLE|__GFP_NORETRY);
 	if (b)
 		return (struct bbuf) { .c = c, .b = b, .type = BB_mempool, .rw = rw };
 
@@ -245,7 +245,7 @@ static int buf_uncompress(struct bch_fs *c,
 			.avail_out	= dst_len,
 		};
 
-		void *workspace = mempool_alloc(workspace_pool, GFP_NOFS|__GFP_NORETRY);
+		void *workspace = mempool_alloc(workspace_pool, GFP_NOFS|__GFP_RECLAIMABLE|__GFP_NORETRY);
 
 		zlib_set_workspace(&strm, workspace);
 		zlib_inflateInit2(&strm, -MAX_WBITS);
@@ -264,7 +264,7 @@ static int buf_uncompress(struct bch_fs *c,
 		if (real_src_len > src_len - 4)
 			return bch_err_throw(c, decompress_zstd_src_len_bad);
 
-		void *workspace = mempool_alloc(workspace_pool, GFP_NOFS|__GFP_NORETRY);
+		void *workspace = mempool_alloc(workspace_pool, GFP_NOFS|__GFP_RECLAIMABLE|__GFP_NORETRY);
 		ctx = zstd_init_dctx(workspace, zstd_dctx_workspace_bound());
 
 		size_t ret = zstd_decompress_dctx(ctx,
@@ -490,7 +490,7 @@ static unsigned bch2_compress(struct bch_fs *c,
 		}
 	}
 
-	void *workspace = mempool_alloc(workspace_pool, GFP_NOFS|__GFP_NORETRY);
+	void *workspace = mempool_alloc(workspace_pool, GFP_NOFS|__GFP_RECLAIMABLE|__GFP_NORETRY);
 
 	/*
 	 * XXX: this algorithm sucks when the compression code doesn't tell us

bhzhu203/bcachefs-tools

fs/data/compress.c
@@ -128,7 +128,7 @@ struct bbuf bch2_bounce_alloc(struct bch_fs *c, unsigned size, int rw)
 	if (b)
 		return (struct bbuf) { .c = c, .b = b, .type = BB_kmalloc, .rw = rw };
 
-	b = mempool_alloc(&c->compress.bounce[rw], GFP_NOIO|__GFP_NORETRY);
+	b = mempool_alloc(&c->compress.bounce[rw], GFP_NOIO|__GFP_RECLAIMABLE|__GFP_NORETRY);
 	if (b)
 		return (struct bbuf) { .c = c, .b = b, .type = BB_mempool, .rw = rw };
 
@@ -277,7 +277,7 @@ int bch2_buf_uncompress(struct bch_fs *c,
 			.avail_out	= dst_len,
 		};
 
-		void *workspace = mempool_alloc(workspace_pool, GFP_NOIO|__GFP_NORETRY);
+		void *workspace = mempool_alloc(workspace_pool, GFP_NOIO|__GFP_RECLAIMABLE|__GFP_NORETRY);
 
 		zlib_set_workspace(&strm, workspace);
 		zlib_inflateInit2(&strm, -MAX_WBITS);
@@ -298,7 +298,7 @@ int bch2_buf_uncompress(struct bch_fs *c,
 		if (real_src_len > src_len - 4)
 			return bch_err_throw(c, decompress_zstd_src_len_bad);
 
-		void *workspace = mempool_alloc(workspace_pool, GFP_NOIO|__GFP_NORETRY);
+		void *workspace = mempool_alloc(workspace_pool, GFP_NOIO|__GFP_RECLAIMABLE|__GFP_NORETRY);
 		ctx = zstd_init_dctx(workspace, zstd_dctx_workspace_bound());
 
 		size_t ret = zstd_decompress_dctx(ctx,
@@ -505,7 +505,7 @@ static unsigned bch2_compress(struct bch_fs *c,
 		}
 	}
 
-	void *workspace = mempool_alloc(workspace_pool, GFP_NOIO|__GFP_NORETRY);
+	void *workspace = mempool_alloc(workspace_pool, GFP_NOIO|__GFP_RECLAIMABLE|__GFP_NORETRY);
 
 	/*
 	 * XXX: this algorithm sucks when the compression code doesn't tell us