bhzhu203/bcachefs

bcachefs: Add __GFP_RECLAIMABLE to all large allocations to fix kcompactd0 compaction failures
The previous __GFP_RECLAIMABLE fix (5a6366a75b0d) was ineffective because
bch2_mm_avoid_compaction=true (default) sent most btree node allocations
directly to __vmalloc() without __GFP_RECLAIMABLE, bypassing the kmalloc
path entirely. This caused 91.7% migration failure rate (373M fail vs 33M
success) and 9 billion pages scanned by kcompactd.

Fix the root cause: in the avoid_compaction path, try kmalloc with
__GFP_RECLAIMABLE|__GFP_NORETRY first (prevents expensive compaction
trigger, places compound pages in RECLAIMABLE pageblocks), then fall back
to __vmalloc with __GFP_RECLAIMABLE.

Also add __GFP_RECLAIMABLE to all other large allocations that were
missing it: btree aux_data, journal buffers (free_buf, in_flight, write
buffer, read buffer), compression bounce buffer, EC stripe buffer,
backpointer verification buffer, buckets_nouse bitmap, bucket_gens array,
journal keys array, and GC oldest_gen array.

This ensures bcachefs-allocated pages are placed in MIGRATE_RECLAIMABLE
pageblocks instead of MIGRATE_UNMOVABLE, allowing kcompactd0 to reclaim
them via the btree cache shrinker when contiguous memory is needed.

fs/bcachefs/journal/journal.c
@@ -661,7 +661,7 @@ static void journal_buf_prealloc(struct journal *j)
 	unsigned buf_size = j->buf_size_want;
 
 	spin_unlock(&j->lock);
-	void *buf = kvmalloc(buf_size, GFP_NOFS);
+	void *buf = kvmalloc(buf_size, GFP_NOFS|__GFP_RECLAIMABLE);
 	spin_lock(&j->lock);
 
 	if (!buf)

fs/bcachefs/journal/write.c
@@ -174,7 +174,7 @@ static void journal_buf_realloc(struct journal *j, struct journal_buf *buf)
 	if (bch2_btree_write_buffer_resize(c, btree_write_buffer_size))
 		return;
 
-	new_buf = kvmalloc(new_size, GFP_NOFS|__GFP_NOWARN);
+	new_buf = kvmalloc(new_size, GFP_NOFS|__GFP_NOWARN|__GFP_RECLAIMABLE);
 	if (!new_buf)
 		return;
 

bhzhu203/bcachefs-tools

fs/journal/journal.c
@@ -780,7 +780,7 @@ static void journal_buf_prealloc(struct journal *j)
 	unsigned buf_size = j->buf_size_want;
 
 	spin_unlock(&j->lock);
-	void *buf = kvmalloc(buf_size, GFP_NOIO);
+	void *buf = kvmalloc(buf_size, GFP_NOIO|__GFP_RECLAIMABLE);
 	spin_lock(&j->lock);
 
 	if (!buf)

fs/journal/write.c
@@ -195,7 +195,7 @@ static void journal_buf_realloc(struct journal *j, struct journal_buf *buf)
 	if (bch2_btree_write_buffer_resize(c, btree_write_buffer_size))
 		return;
 
-	new_buf = kvmalloc(new_size, GFP_NOIO|__GFP_NOWARN);
+	new_buf = kvmalloc(new_size, GFP_NOIO|__GFP_NOWARN|__GFP_RECLAIMABLE);
 	if (!new_buf)
 		return;
 