bhzhu203/bcachefs

bcachefs: Add __GFP_RECLAIMABLE to remaining hot path allocations and darray defaults
Add __GFP_RECLAIMABLE to ~100+ allocation sites across 31 files covering
all remaining high and medium frequency UNMOVABLE allocation paths:

- vfs/pagecache.c: __bch2_folio_create internal gfp (covers all bch_folio
  allocations, one per page cache folio — the largest hidden UNMOVABLE source)
- btree/iter.c: __bch2_trans_kmalloc primary kmalloc (hottest path, used by
  every btree update) and trans mempool
- vfs/buffered.c, vfs/direct.c, vfs/io.c: all read/write/direct IO bios,
  writepage state, folio reservation, nocow flush
- data/read.c, data/write.c, data/ec/: read bounce/clone, write replica/
  new/split, EC stripe bios, promote_op, keylist, data_update bvecs
- btree/read.c, btree/write.c, journal/write.c: btree node and journal bios
- journal/init.c: pin fifo, bucket_seq, buckets arrays
- btree/interior.c, btree/bkey_buf.h, btree/check.c: interior_updates
  mempool, async_btree_rewrite, bkey_buf fallback, gc buckets
- util/darray.h: change default gfp for darray_push/darray_make_room/
  darray_resize/darray_resize_rcu macros to GFP_KERNEL|__GFP_RECLAIMABLE
  (blanket coverage for write_buffer, btree interior, journal_overlay,
  accounting, key_cache, and all other darray callers)
- init/fs.c, init/dev.c: bch_fs and bch_dev struct allocations
- alloc/replicas.c, alloc/backpointers.c, fs/quota.c, snapshots/snapshot.c,
  data/copygc.c, data/reflink.c, data/nocow_locking.c, util/six.c

__GFP_RECLAIMABLE only changes pageblock migratetype from MIGRATE_UNMOVABLE
to MIGRATE_RECLAIMABLE without altering allocation behavior (retry, reclaim,
IO/FS allowances). This enables kcompactd to migrate these pages during
compaction, reducing order-9 fragmentation that causes I/O storms on HDD.

fs/bcachefs/btree/iter.c
@@ -3567,7 +3567,7 @@ void *__bch2_trans_kmalloc(struct btree_trans *trans, size_t size, unsigned long
 
 	bool lock_dropped = false;
 	new_mem = allocate_dropping_locks_norelock(trans, lock_dropped,
-					kmalloc(new_bytes, _gfp|__GFP_NOWARN|__GFP_NORETRY));
+					kmalloc(new_bytes, _gfp|__GFP_NOWARN|__GFP_NORETRY|__GFP_RECLAIMABLE));
 	if (!new_mem) {
 		new_mem = mempool_alloc(&c->btree.trans.malloc_pool, GFP_KERNEL|__GFP_RECLAIMABLE|__GFP_NORETRY);
 		new_bytes = BTREE_TRANS_MEM_MAX;
@@ -3753,7 +3753,7 @@ static inline struct btree_trans *bch2_trans_alloc(struct bch_fs *c)
 		}
 	}
 
-	struct btree_trans *trans = mempool_alloc(&c->btree.trans.pool, GFP_NOFS);
+	struct btree_trans *trans = mempool_alloc(&c->btree.trans.pool, GFP_NOFS|__GFP_RECLAIMABLE);
 	memset(trans, 0, sizeof(*trans));
 
 	seqmutex_lock(&c->btree.trans.lock);
@@ -3925,7 +3925,7 @@ skip_throttle:
 		if (s->max_mem) {
 			unsigned expected_mem_bytes = roundup_pow_of_two(s->max_mem);
 
-			trans->mem = kmalloc(expected_mem_bytes, GFP_KERNEL|__GFP_NOWARN|__GFP_NORETRY);
+			trans->mem = kmalloc(expected_mem_bytes, GFP_KERNEL|__GFP_NOWARN|__GFP_NORETRY|__GFP_RECLAIMABLE);
 			if (likely(trans->mem))
 				trans->mem_bytes = expected_mem_bytes;
 		}

fs/bcachefs/vfs/buffered.c
@@ -329,7 +329,8 @@ void bch2_readahead(struct readahead_control *ractl)
 				   BIO_MAX_VECS);
 		struct bch_read_bio *rbio =
 			rbio_init(bio_alloc_bioset(NULL, n, REQ_OP_READ,
-						   GFP_KERNEL, &c->bio_read),
+						   GFP_KERNEL|__GFP_RECLAIMABLE,
+						   &c->bio_read),
 				  c,
 				  opts,
 				  bch2_readpages_end_io);
@@ -374,7 +375,9 @@ int bch2_read_single_folio(struct folio *folio, struct address_space *mapping)
 
 	bch2_inode_opts_get_inode(c, &inode->ei_inode, &opts);
 
-	rbio = rbio_init(bio_alloc_bioset(NULL, 1, REQ_OP_READ, GFP_KERNEL, &c->bio_read),
+	rbio = rbio_init(bio_alloc_bioset(NULL, 1, REQ_OP_READ,
+					  GFP_KERNEL|__GFP_RECLAIMABLE,
+					  &c->bio_read),
 			 c,
 			 opts,
 			 bch2_read_single_folio_end_io);
@@ -516,7 +519,7 @@ static void bch2_writepage_io_alloc(struct bch_fs *c,
 
 	w->io = container_of(bio_alloc_bioset(NULL, BIO_MAX_VECS,
 					      REQ_OP_WRITE,
-					      GFP_KERNEL,
+					      GFP_KERNEL|__GFP_RECLAIMABLE,
 					      &c->vfs.writepage_bioset),
 			     struct bch_writepage_io, op.wbio.bio);
 
@@ -738,9 +741,9 @@ do_io:
 int bch2_writepages(struct address_space *mapping, struct writeback_control *wbc)
 {
 	struct bch_fs *c = mapping->host->i_sb->s_fs_info;
-	struct bch_writepage_state *w = kzalloc(sizeof(*w), GFP_NOFS|__GFP_NOFAIL);
+	struct bch_writepage_state *w = kzalloc(sizeof(*w), GFP_NOFS|__GFP_NOFAIL|__GFP_RECLAIMABLE);
 
-	w->tmp = mempool_alloc(&c->vfs.writepage_buf_pool, GFP_NOFS);
+	w->tmp = mempool_alloc(&c->vfs.writepage_buf_pool, GFP_NOFS|__GFP_RECLAIMABLE);
 
 	bch2_inode_opts_get_inode(c, &to_bch_ei(mapping->host)->ei_inode, &w->opts);
 
@@ -782,7 +785,7 @@ int bch2_write_begin(
 	unsigned offset;
 	int ret = -ENOMEM;
 
-	res = kmalloc(sizeof(*res), GFP_KERNEL);
+	res = kmalloc(sizeof(*res), GFP_KERNEL|__GFP_RECLAIMABLE);
 	if (!res)
 		return -ENOMEM;
 

bhzhu203/bcachefs-tools

fs/btree/iter.c
@@ -3753,7 +3753,7 @@ void *__bch2_trans_kmalloc(struct btree_trans *trans, size_t size, unsigned long
 	EBUG_ON(new_bytes > BTREE_TRANS_MEM_MAX);
 
 	new_mem = allocate_dropping_locks_norelock(trans,
-					kmalloc(new_bytes, _gfp|__GFP_NOWARN|__GFP_NORETRY));
+					kmalloc(new_bytes, _gfp|__GFP_NOWARN|__GFP_NORETRY|__GFP_RECLAIMABLE));
 	if (!new_mem) {
 		new_mem = mempool_alloc(&c->btree.trans.malloc_pool, GFP_KERNEL|__GFP_RECLAIMABLE|__GFP_NORETRY);
 		new_bytes = BTREE_TRANS_MEM_MAX;
@@ -3965,7 +3965,7 @@ static inline struct btree_trans *bch2_trans_alloc(struct bch_fs *c)
 		}
 	}
 
-	struct btree_trans *trans = mempool_alloc(&c->btree.trans.pool, GFP_NOIO);
+	struct btree_trans *trans = mempool_alloc(&c->btree.trans.pool, GFP_NOIO|__GFP_RECLAIMABLE);
 	memset(trans, 0, sizeof(*trans));
 
 	seqmutex_lock(&c->btree.trans.lock);
@@ -4165,7 +4165,7 @@ skip_throttle:
 		if (s->max_mem) {
 			unsigned expected_mem_bytes = roundup_pow_of_two(s->max_mem);
 
-			trans->mem = kmalloc(expected_mem_bytes, GFP_KERNEL|__GFP_NOWARN|__GFP_NORETRY);
+			trans->mem = kmalloc(expected_mem_bytes, GFP_KERNEL|__GFP_NOWARN|__GFP_NORETRY|__GFP_RECLAIMABLE);
 			if (likely(trans->mem))
 				trans->mem_bytes = expected_mem_bytes;
 		}

fs/vfs/buffered.c
@@ -444,7 +444,8 @@ void bch2_readahead(struct readahead_control *ractl)
 				   BIO_MAX_VECS);
 		struct bch_read_bio *rbio =
 			rbio_init(bio_alloc_bioset(NULL, n, REQ_OP_READ,
-						   GFP_KERNEL, &c->bio_read),
+						   GFP_KERNEL|__GFP_RECLAIMABLE,
+						   &c->bio_read),
 				  c,
 				  opts,
 				  bch2_readpages_end_io);
@@ -490,7 +491,9 @@ int bch2_read_single_folio(struct folio *folio, struct address_space *mapping)
 
 	bch2_inode_opts_get_inode(c, &inode->ei_inode, &opts);
 
-	rbio = rbio_init(bio_alloc_bioset(NULL, 1, REQ_OP_READ, GFP_KERNEL, &c->bio_read),
+	rbio = rbio_init(bio_alloc_bioset(NULL, 1, REQ_OP_READ,
+					  GFP_KERNEL|__GFP_RECLAIMABLE,
+					  &c->bio_read),
 			 c,
 			 opts,
 			 bch2_read_single_folio_end_io);
@@ -633,7 +636,7 @@ static void bch2_writepage_io_alloc(struct bch_fs *c,
 
 	w->io = container_of(bio_alloc_bioset(NULL, BIO_MAX_VECS,
 					      REQ_OP_WRITE,
-					      GFP_KERNEL,
+					      GFP_KERNEL|__GFP_RECLAIMABLE,
 					      &c->vfs.writepage_bioset),
 			     struct bch_writepage_io, op.wbio.bio);
 
@@ -855,9 +858,9 @@ do_io:
 int bch2_writepages(struct address_space *mapping, struct writeback_control *wbc)
 {
 	struct bch_fs *c = mapping->host->i_sb->s_fs_info;
-	struct bch_writepage_state *w = kzalloc(sizeof(*w), GFP_NOIO|__GFP_NOFAIL);
+	struct bch_writepage_state *w = kzalloc(sizeof(*w), GFP_NOIO|__GFP_NOFAIL|__GFP_RECLAIMABLE);
 
-	w->tmp = mempool_alloc(&c->vfs.writepage_buf_pool, GFP_NOIO);
+	w->tmp = mempool_alloc(&c->vfs.writepage_buf_pool, GFP_NOIO|__GFP_RECLAIMABLE);
 
 	bch2_inode_opts_get_inode(c, &to_bch_ei(mapping->host)->ei_inode, &w->opts);
 
@@ -899,7 +902,7 @@ int bch2_write_begin(
 	unsigned offset;
 	int ret = -ENOMEM;
 
-	res = kmalloc(sizeof(*res), GFP_KERNEL);
+	res = kmalloc(sizeof(*res), GFP_KERNEL|__GFP_RECLAIMABLE);
 	if (!res)
 		return -ENOMEM;
 